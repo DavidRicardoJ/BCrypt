e8b11a0775b306b8133e4e50acbac80c71c7849a
José Ricardo David <David_Ricardo@live.com>
2020-08-21 15:03:29 -0300

    Example of using BCrypt in passwords with C #

---
+++ Crypto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt.Net;
+
+namespace BCrypt
+{
+    static class Crypto
+    {
+
+        private static string GetRandomSalt()
+        {
+            return Net.BCrypt.GenerateSalt(12);
+        }
+
+        public static string HashPassword(string password)
+        {
+            return Net.BCrypt.HashPassword(password, GetRandomSalt());
+        }
+
+        public static bool ValidatePassword(string password, string correctHash)
+        {
+            return Net.BCrypt.Verify(password, correctHash);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BCrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            Console.Write("Nome: ");
+            user.Nome = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Email: ");
+            user.Email = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Senha: ");
+            user.Senha = Console.ReadLine();
+
+            Console.WriteLine();
+            user.Senha = Crypto.HashPassword(user.Senha);
+            Console.WriteLine($"Nome: {user.Nome}");
+            Console.WriteLine($"Email: {user.Email}");
+            Console.WriteLine($"Senha: {user.Senha}");
+
+            Console.WriteLine();
+            Console.Write("Digite sua senha novamente: ");
+            var senha = Console.ReadLine();
+            Console.WriteLine();
+            if (Crypto.ValidatePassword(senha, user.Senha))
+            {
+                Console.WriteLine("As senhas são iguais.");
+            }
+            else
+            {
+                Console.WriteLine("As senhas são diferentes.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
